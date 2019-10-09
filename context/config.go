a33fe0ceaae58615935c2d0f730e907cd5265f3c
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2019-10-09 23:31:36 +0200

    Add basic http handling

--- context/config.go
+++ context/config.go
@@ -13,16 +13,22 @@ const (
 
 	LogLevelKey = configRoot + "log-level"
 	StateStoreKey = configRoot + "state-store"
+	
+	web = configRoot + "webserver."
+	WebListenKey = web + "listen"
+	WebPortKey = web + "port"
 )
 
 func ReadConfig() *viper.Viper {
 	viper := viper.GetViper()
 	viper.SetConfigType("yaml")
-	viper.SetConfigName(".go-short")
+	viper.SetConfigName("go-short")
 	viper.AddConfigPath("$HOME/.go-short")
 
 	viper.SetDefault(LogLevelKey, "info")
 	viper.SetDefault(StateStoreKey, "~/.go-short/state-store")
+	viper.SetDefault(WebListenKey, "localhost")
+	viper.SetDefault(WebPortKey, "80")
 	
 	if err := viper.ReadInConfig(); err != nil {
 		panic(fmt.Errorf("Fatal error: %s\n", err))
--- main.go
+++ main.go
@@ -1,17 +1,98 @@
 package main
 
 import (
+	"flag"
+	"os"
+	"fmt"
+	"net/http"
+	"strings"
+	
 	"github.com/kouzant/go-short/context"
 	"github.com/kouzant/go-short/logger"
 	"github.com/kouzant/go-short/storage"	
 	log "github.com/sirupsen/logrus"	
 )
 
+
 func main() {
+	serverMode := flag.NewFlagSet("server", flag.ExitOnError)
+	clientMode := flag.NewFlagSet("client", flag.ExitOnError)
+	
+	// Client mode arguments
+	opArg := clientMode.String("operation", "add", "Operation (add | delete)")
+	keyArg := clientMode.String("key", "", "Shortened URL key")
+	valueArg := clientMode.String("url", "", "URL")
+
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s [server | client] ...\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "server":
+		serverMode.Parse(os.Args[2:])
+	case "client":
+		clientMode.Parse(os.Args[2:])
+	default:
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	
 	conf := context.ReadConfig()
 	logger.Init(conf)
 	log.Info("Starting go-short")
 
 	stateStore := &storage.MemoryStateStore{Config: conf}
 	stateStore.Init()
+	defer stateStore.Close()
+
+	item := &storage.StorageItem{
+		storage.StorageKey("lala"),
+		storage.StorageValue("https://logicalclocks.com")}
+	stateStore.Save(item)
+	
+	if serverMode.Parsed() {
+		fmt.Println("Server parsed")
+		mux := http.NewServeMux()
+		rh := &RedirectHandler{stateStore: stateStore}
+		mux.Handle("/", rh)
+		listeningOn := fmt.Sprintf("%s:%d", conf.GetString(context.WebListenKey),
+			conf.GetInt(context.WebPortKey))
+		http.ListenAndServe(listeningOn, mux)
+	} else if clientMode.Parsed() {
+		fmt.Println("Client parsed")		
+		switch *opArg {
+		case "add":
+			fmt.Println("op add")
+			if *keyArg == "" || *valueArg == "" {
+				clientMode.PrintDefaults()
+				os.Exit(1)
+			}
+			fmt.Printf("op: add key: %s url: %s\n", *keyArg, *valueArg)
+		case "delete":
+			fmt.Println("op delete")
+			if *keyArg == "" {
+				clientMode.PrintDefaults()
+				os.Exit(2)
+			}
+			fmt.Printf("op delete key: %s\n", *keyArg)
+		default:
+			clientMode.PrintDefaults()
+			os.Exit(1)
+		}
+	}
+}
+
+type RedirectHandler struct {
+	stateStore storage.StateStore
+}
+
+func (h *RedirectHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	tokens := strings.SplitAfterN(r.URL.Path, "/", 2)
+	value, error := h.stateStore.Load(storage.StorageKey(tokens[1]))
+	if error != nil {
+		fmt.Fprintf(w, "Error: %v", error)
+	} else {
+		http.Redirect(w, r, value.(string), http.StatusMovedPermanently)
+	}
 }
