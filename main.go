0004692bfbff9fcf268b87071432e86fd0031eb9
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2019-10-09 21:42:34 +0200

    Remove some code

--- main.go
+++ main.go
@@ -12,9 +12,6 @@ func main() {
 	logger.Init(conf)
 	log.Info("Starting go-short")
 
-	//stateStore := &storage.StateStore{Config: conf}
 	stateStore := &storage.MemoryStateStore{Config: conf}
 	stateStore.Init()
-	write(stateStore, "antonis", "kouzoupis")
-	load(stateStore, "antoniss")
 }
